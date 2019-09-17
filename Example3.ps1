f4e16252faecc8e78d368901bdaf20bca3277c93
C. David Littlejohn <cdlittlejohn1@gmail.com>
2019-09-17 19:22:35 -0400

    Update to throwing errors

---
+++ Example3.ps1
@@ -0,0 +1,26 @@
+# Throwing an error to catch
+
+try {
+    # obligatory clearing of error variable for example
+    $Error.Clear()
+
+    Write-Output "This has some fancy code."
+    # minding
+    # my own
+    # business
+    Write-Output "I dont like what I see and" +
+        "should handle it seperately when I see it"
+
+    # simulating identifying an edge case
+    if ($true){
+        throw "This doesn't seem right"
+    }
+} catch{
+    $Error[0]
+    # $Error[0].CategoryInfo # Info about reason and category
+    # $Error[0].Exception # Extracted issue
+    # $Error[0].InvocationInfo # Finding the culprit
+    # $Error[0].InvocationInfo.ScriptName # Finding the culprit; curated
+    # $Error[0].ScriptStackTrace # Finding the needle in the hay stack
+    # $Error[0].Exception.GetType().FullName # Segway into catching specfic error
+}
